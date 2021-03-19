413934245257cd0a68865b6cf9bd3732cc4db041
Nayara Gomes <nayyara_nivea@hotmail.com>
2021-03-19 11:51:07 +0100

    Endpoint /clientes/{id} disponivel

--- src/main/java/com/nayaragomes/cursomc/CursomcApplication.java
+++ src/main/java/com/nayaragomes/cursomc/CursomcApplication.java
@@ -87,7 +87,7 @@ public class CursomcApplication implements CommandLineRunner{
 		
 		Endereco e2 = new Endereco(null, "Avenida Matos", "105", "Sala 800", "Centro", "38777012", cli1, c2);
 		
-		cli1.getEnderecos().addAll(Arrays.asList());
+		cli1.getEnderecos().addAll(Arrays.asList(e1, e2));
 		
 		clienteRepository.saveAll(Arrays.asList(cli1));
 		enderecoRepository.saveAll(Arrays.asList(e1, e2));
--- src/main/java/com/nayaragomes/cursomc/domain/Endereco.java
+++ src/main/java/com/nayaragomes/cursomc/domain/Endereco.java
@@ -9,6 +9,8 @@ import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
 
+import com.fasterxml.jackson.annotation.JsonBackReference;
+
 @Entity
 public class Endereco implements Serializable{
 	private static final long serialVersionUID = 1L;
@@ -23,6 +25,7 @@ public class Endereco implements Serializable{
 	private String bairro;
 	private String cep;
 	
+	@JsonBackReference
 	@ManyToOne
 	@JoinColumn(name="cliente_id")
 	private Cliente cliente;
@@ -43,6 +46,7 @@ public class Endereco implements Serializable{
 		this.bairro = bairro;
 		this.cep = cep;
 		this.cliente = cliente;
+		this.cidade = cidade;
 	}
 
 	public Integer getId() {
