e30f72d0b2a05478599ab2f53c34e8d52b755c99
Nayara Gomes <nayyara_nivea@hotmail.com>
2021-03-24 12:16:56 +0100

    Listando todas categorias com DTO

--- src/main/java/com/nayaragomes/cursomc/services/CategoriaService.java
+++ src/main/java/com/nayaragomes/cursomc/services/CategoriaService.java
@@ -1,5 +1,6 @@
 package com.nayaragomes.cursomc.services;
 
+import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -41,5 +42,9 @@ public class CategoriaService {
 		catch (DataIntegrityViolationException e){
 			throw new DataIntegrityException("Não é possível excluir uma categoria com produtos");
 		}
+	}
+	
+	public List<Categoria> findAll() {
+		return repo.findAll();
 	}
 }
